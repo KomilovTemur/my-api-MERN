acb27c0b550ff38bd24025257f80dfa0c470a5ab
Tim <itteamdev74@gmail.com>
2022-12-14 10:31:41 +0500

    sign up route added for creeating new users

--- routes/admin.js
+++ routes/admin.js
@@ -5,6 +5,34 @@ require("dotenv").config();
 
 const Admin = require("../model/Admin");
 
+// Creating new admin || user
+router.post("/signUp", async (req, res) => {
+  try {
+    const { login, password } = req.body;
+    const findLogin = await Admin.find({ login });
+    if (findLogin.length === 0) {
+      const admin = await new Admin({ login, password });
+      await admin.save();
+      const jwtData = {
+        login,
+        password,
+      };
+      const token = jwt.sign(jwtData, process.env.SECRETKEY, {
+        expiresIn: "24h",
+      });
+      res.cookie("authorization", token, {
+        maxAge: 24 * 60 * 60 * 1000,
+        httpOnly: true,
+      });
+      res.json(admin);
+    } else {
+      res.json({ massage: "This login already taken" });
+    }
+  } catch (err) {
+    console.log(err);
+  }
+});
+
 router.post("/login", async (req, res) => {
   const { login, password } = req.body;
   const admin = await Admin.find({ login, password });
