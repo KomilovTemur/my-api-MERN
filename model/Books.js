47b92e63e31f6238c3a42c8897eee494ac9f66f9
tim_coder <itteamdev74@gmail.com>
2022-12-21 14:06:04 +0500

    some changes

--- model/Books.js
+++ model/Books.js
@@ -1,5 +1,7 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+mongoose.set('strictQuery', true)
+
 const BooksSchema = new Schema({
   title: { type: String, required: true },
   isbn: String,
